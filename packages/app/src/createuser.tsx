c4963c398adcbe7ec02c4acb1f7fa8fdbf085953
carson peterson <carsonpetersoncar@yahoo.com>
2022-10-24 11:25:04 -0700

    add basic routing + auth pages

---
+++ packages/app/src/createuser.tsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+  
+const CreteUser = () => {
+  return (
+    <div style={{display: "flex", justifyContent: "center"}}>
+<h1>
+        This is where you sign Up
+      </h1>
+    </div>
+  );
+};
+  
+export default CreteUser;

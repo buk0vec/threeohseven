f880d25ba156bde7fbe9e0521ff8006cca9ae9dc
arthurz12345 <arthurz12345@gmail.com>
2022-11-07 11:41:07 -0800

    Add about page and home

---
+++ packages/app/src/about.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Button, Typography, Grid, Box, TextField } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+
+
+const About = () => {
+    return (
+        <div>
+
+        <Box
+        display="flex"
+        flexDirection={"column"}
+        maxWidth={800}
+        maxHeight={800}
+        alignItems="center"
+        justifyContent={"center"}
+        margin="auto"
+        marginTop={0}
+        padding={5}
+        borderRadius={10}
+        boxShadow={"10px 10px 20px #ccc"}
+        sx={{
+            ":hoover":{
+                boxShadow: "15px 15px 25px #ccc",
+            },
+        }}
+        >
+            <h1>About Us</h1>   
+
+            <img src="/src/images/bush2.png"></img>    
+             
+             <h3>We're the little guys going up against the big guys at LinkTree.<br></br>Support small businesses</h3>
+          <Button
+            sx={{marginTop: 3, borderRadius:3 }}
+            variant="contained"
+            color='primary'
+            component={Link} to={'/'}
+            >
+                Back To Home
+          </Button>  
+        </Box>
+        
+        </div>
+
+      
+    );
+  };
+    
+  export default About;
